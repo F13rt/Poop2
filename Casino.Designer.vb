1cb19e3601b1d61f5a50c07f22cd6c5ce67adece
Tyler Mielke <tmielke1@icloud.com>
2025-06-04 07:38:24 -0400

    Fixed a resizing issue due yo how small my laptops screen is compared to my pcs.

--- Casino.Designer.vb
+++ Casino.Designer.vb
@@ -39,19 +39,22 @@ Partial Class Casino
         '
         Me.BTNexit.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.BTNexit.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BTNexit.Location = New System.Drawing.Point(1485, 847)
+        Me.BTNexit.Location = New System.Drawing.Point(1463, 954)
+        Me.BTNexit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.BTNexit.Name = "BTNexit"
-        Me.BTNexit.Size = New System.Drawing.Size(234, 55)
+        Me.BTNexit.Size = New System.Drawing.Size(316, 42)
         Me.BTNexit.TabIndex = 0
         Me.BTNexit.Text = "E&xit"
         Me.BTNexit.UseVisualStyleBackColor = False
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Menu})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(1731, 33)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1792, 50)
         Me.MenuStrip1.TabIndex = 1
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -60,28 +63,29 @@ Partial Class Casino
         Me.Menu.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DarkMode, Me.LightMode})
         Me.Menu.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Menu.Name = "Menu"
-        Me.Menu.Size = New System.Drawing.Size(129, 29)
+        Me.Menu.Size = New System.Drawing.Size(197, 44)
         Me.Menu.Text = "Menu Items"
         '
         'DarkMode
         '
         Me.DarkMode.Name = "DarkMode"
-        Me.DarkMode.Size = New System.Drawing.Size(186, 30)
+        Me.DarkMode.Size = New System.Drawing.Size(281, 48)
         Me.DarkMode.Text = "Dark Mode"
         '
         'LightMode
         '
         Me.LightMode.Name = "LightMode"
-        Me.LightMode.Size = New System.Drawing.Size(186, 30)
+        Me.LightMode.Size = New System.Drawing.Size(281, 48)
         Me.LightMode.Text = "Light Mode"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe Script", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 33)
+        Me.Label1.Location = New System.Drawing.Point(18, 51)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(900, 102)
+        Me.Label1.Size = New System.Drawing.Size(1347, 153)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "DrUnk DRiving Sim 2.0"
         '
@@ -89,9 +93,10 @@ Partial Class Casino
         '
         Me.DriveHome.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.DriveHome.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.DriveHome.Location = New System.Drawing.Point(1485, 786)
+        Me.DriveHome.Location = New System.Drawing.Point(1463, 902)
+        Me.DriveHome.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.DriveHome.Name = "DriveHome"
-        Me.DriveHome.Size = New System.Drawing.Size(234, 55)
+        Me.DriveHome.Size = New System.Drawing.Size(316, 42)
         Me.DriveHome.TabIndex = 3
         Me.DriveHome.Text = "Drive Home"
         Me.DriveHome.UseVisualStyleBackColor = False
@@ -100,9 +105,10 @@ Partial Class Casino
         '
         Me.Bathroom.BackColor = System.Drawing.Color.Yellow
         Me.Bathroom.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Bathroom.Location = New System.Drawing.Point(1485, 725)
+        Me.Bathroom.Location = New System.Drawing.Point(1463, 850)
+        Me.Bathroom.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Bathroom.Name = "Bathroom"
-        Me.Bathroom.Size = New System.Drawing.Size(234, 55)
+        Me.Bathroom.Size = New System.Drawing.Size(316, 42)
         Me.Bathroom.TabIndex = 4
         Me.Bathroom.Text = "Go To The Bathroom"
         Me.Bathroom.UseVisualStyleBackColor = False
@@ -111,9 +117,10 @@ Partial Class Casino
         '
         Me.Reset.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.Reset.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Reset.Location = New System.Drawing.Point(1245, 847)
+        Me.Reset.Location = New System.Drawing.Point(1139, 954)
+        Me.Reset.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Reset.Name = "Reset"
-        Me.Reset.Size = New System.Drawing.Size(234, 55)
+        Me.Reset.Size = New System.Drawing.Size(316, 42)
         Me.Reset.TabIndex = 5
         Me.Reset.Text = "Reset"
         Me.Reset.UseVisualStyleBackColor = False
@@ -124,18 +131,19 @@ Partial Class Casino
         Me.MoneyCounter.AutoSize = True
         Me.MoneyCounter.Font = New System.Drawing.Font("Yu Gothic", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.MoneyCounter.ForeColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.MoneyCounter.Location = New System.Drawing.Point(1570, 49)
+        Me.MoneyCounter.Location = New System.Drawing.Point(2355, 75)
+        Me.MoneyCounter.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.MoneyCounter.Name = "MoneyCounter"
-        Me.MoneyCounter.Size = New System.Drawing.Size(149, 27)
+        Me.MoneyCounter.Size = New System.Drawing.Size(227, 42)
         Me.MoneyCounter.TabIndex = 6
         Me.MoneyCounter.Text = "Money: $1000"
         '
         'Casino
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Gray
-        Me.ClientSize = New System.Drawing.Size(1731, 914)
+        Me.ClientSize = New System.Drawing.Size(1792, 1018)
         Me.Controls.Add(Me.MoneyCounter)
         Me.Controls.Add(Me.Reset)
         Me.Controls.Add(Me.Bathroom)
@@ -144,6 +152,7 @@ Partial Class Casino
         Me.Controls.Add(Me.BTNexit)
         Me.Controls.Add(Me.MenuStrip1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Casino"
         Me.Text = "Casino"
         Me.MenuStrip1.ResumeLayout(False)
